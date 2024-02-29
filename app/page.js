b4dce470aa13043cc65899685edd9aabb0dd2625
Ndukwe Chima <chimandukwe.r@gmail.com>
2024-02-29 11:28:22 +0100

    Created the project and added shadcn

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="">
+        <h1 className="text-red-500">HELLO WORLD</h1>
+
+        <Button>Click me!</Button>
+      </div>
+    </main>
+  );
+}
